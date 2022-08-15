ff0847d829bd4a76a02ec240f19a33179201e345
Star-Squid <81830542+Star-Squid@users.noreply.github.com>
2022-08-15 20:46:41 +0100

    improve post styling, remove unnecessary divs

--- src/templates/blogpost.js
+++ src/templates/blogpost.js
@@ -3,6 +3,7 @@ import Layout from "../components/layout"
 import PostDate from "./postdate"
 import PostNav from "./postnav"
 import { graphql } from "gatsby"
+import * as blogPostStyles from "./blogpoststyles.module.scss"
 
 //needed to target each individual post (target by the known slug)
 export const query = graphql`
@@ -20,14 +21,13 @@ export const query = graphql`
 export default function BlogPost(props) {
   return (
     <Layout>
-      <div class="post">
+      
         <PostDate>{props.data.contentfulBlogPost.publishedDate}</PostDate>
-        <h2>{props.data.contentfulBlogPost.title}</h2>
+        <h2 className={blogPostStyles.title}>{props.data.contentfulBlogPost.title}</h2>
         <p>
         {props.data.contentfulBlogPost.body.raw}
         </p>
-      </div>
-
+      
       <PostNav></PostNav>
     </Layout>
   )
--- src/templates/postdate.js
+++ src/templates/postdate.js
@@ -1,8 +1,8 @@
 import React from "react"
-
+import * as blogPostStyles from "./blogpoststyles.module.scss"
 
 export default function PostDate(props) {
   return (
-    <p class="date"><small>{props.children}</small></p>
+    <p className={blogPostStyles.date}><small>{props.children}</small></p>
   )
 }
