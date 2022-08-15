855a5ba244956d1cad35f093946008c2b493efbf
Star-Squid <81830542+Star-Squid@users.noreply.github.com>
2022-08-15 14:32:10 +0100

    nicer formatting

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,17 +1,43 @@
 import React from "react"
-import {Link} from "gatsby"
+import { Link } from "gatsby"
 import * as navigationStyles from "./navigation.module.scss"
 
 export default function Navigation() {
   return (
-<nav>
-      <div className={navigationStyles.navItem}><Link to="/" className={navigationStyles.link} activeClassName={navigationStyles.activeNavItem}>HOME</Link></div>
+    <nav>
+      <div className={navigationStyles.navItem}>
+        <Link
+          to="/"
+          className={navigationStyles.link}
+          activeClassName={navigationStyles.activeNavItem}
+        >
+          HOME
+        </Link>
+      </div>
       <hr />
-      <div className={navigationStyles.navItem}><Link to="/about" className={navigationStyles.link} activeClassName={navigationStyles.activeNavItem}>ABOUT</Link></div>
+      <div className={navigationStyles.navItem}>
+        <Link
+          to="/about"
+          className={navigationStyles.link}
+          activeClassName={navigationStyles.activeNavItem}
+        >
+          ABOUT
+        </Link>
+      </div>
       <hr />
-      <div className={navigationStyles.navItem}><Link to="/contact" className={navigationStyles.link} activeClassName={navigationStyles.activeNavItem}>CONTACT</Link></div>
+      <div className={navigationStyles.navItem}>
+        <Link
+          to="/contact"
+          className={navigationStyles.link}
+          activeClassName={navigationStyles.activeNavItem}
+        >
+          CONTACT
+        </Link>
+      </div>
       <hr />
-      <div className={navigationStyles.navItem}><h2 className={navigationStyles.link}>☼</h2></div>
+      <div className={navigationStyles.navItem}>
+        <h2 className={navigationStyles.link}>☼</h2>
+      </div>
     </nav>
   )
 }
