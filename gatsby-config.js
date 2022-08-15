1015b9136b0e4290556332827d2979eb3c51d0de
Star-Squid <81830542+Star-Squid@users.noreply.github.com>
2022-08-15 16:53:01 +0100

    post titles import from contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,25 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: `Star Squid Blog`,
+    description: `I post random thoughts as an excuse to play with web development concepts`,
+    author: `Barbara Malta`,
+  },
+
   plugins: [
-    `gatsby-plugin-sass`
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+    `gatsby-plugin-image`,
+    `gatsby-transformer-remark`
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,47 @@
 import React from "react"
 import Layout from "../components/layout"
+import { graphql, useStaticQuery } from "gatsby"
+import { Link } from "gatsby"
+import * as postStyles from "../components/poststyles.module.scss"
 
 export default function IndexPage() {
+  const data = useStaticQuery(graphql`
+    query {
+      allContentfulBlogPost(sort: { fields: publishedDate, order: DESC }) {
+        edges {
+          node {
+            slug
+            title
+            publishedDate(formatString: "D MMM YYYY")
+            body {
+              raw
+            }
+          }
+        }
+      }
+    }
+  `)
+
   return (
     <Layout>
       <h3>Latest posts</h3>
-      <p>Posts will appear here</p>
+
+
+        {data.allContentfulBlogPost.edges.map(edge => {
+          return (
+            <div className={postStyles.vignette}>
+            <Link to={edge.node.slug}>
+              <div className={postStyles.singlepost}>
+               
+                  <p>{edge.node.publishedDate}</p>
+                  <h2>{edge.node.title}</h2>
+                
+              </div>
+            </Link>
+            </div>
+          )
+        })}
+ 
     </Layout>
   )
 }
