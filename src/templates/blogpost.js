ce68245aa99d490bc910dcfcb92fc021b65cf132
Star-Squid <81830542+Star-Squid@users.noreply.github.com>
2022-09-14 18:13:32 +0100

    working post navigation

--- src/templates/blogpost.js
+++ src/templates/blogpost.js
@@ -1,8 +1,8 @@
 import React from "react"
 import Layout from "../components/layout"
 import PostDate from "./postdate"
-// import PostNav from "./postnav"
-import { graphql } from "gatsby"
+import PostNav from "./postnav"
+import { graphql, useStaticQuery } from "gatsby"
 import * as blogPostStyles from "./blogpoststyles.module.scss"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import { Seo } from "../components/seo"
@@ -13,19 +13,61 @@ export const query = graphql`
   query ($slug: String!) {
     contentfulBlogPost(slug: { eq: $slug }) {
       title
+      slug
       publishedDate(formatString: "DD MMM YYYY")
       body {
         raw
+      }
+    }
+
+    allContentfulBlogPost(sort: { order: DESC, fields: publishedDate }) {
+      nodes {
+        title
+        slug
       }
     }
   }
 `
 
 export default function BlogPost(props) {
+  let thisPostNode
+  let thisSlug = props.data.contentfulBlogPost.slug
+  let numberOfPosts = props.data.allContentfulBlogPost.nodes.length
+
+  //find this page's position in the order of nodes
+  for (var i = 0; i < numberOfPosts; i++) {
+    if (props.data.allContentfulBlogPost.nodes[i].slug === thisSlug) {
+      thisPostNode = i
+      break
+    }
+  }
+
+  var oldnode, newnode
+  if (thisPostNode === 0) {
+    newnode = 0
+  } else {
+    newnode = thisPostNode - 1
+  }
+
+  if (thisPostNode === numberOfPosts - 1) {
+    oldnode = numberOfPosts - 1
+  } else {
+    oldnode = thisPostNode + 1
+  }
+
+  var oldslug = props.data.allContentfulBlogPost.nodes[oldnode].slug
+  var newslug = props.data.allContentfulBlogPost.nodes[newnode].slug
+  var oldtitle = props.data.allContentfulBlogPost.nodes[oldnode].title
+  var newtitle = props.data.allContentfulBlogPost.nodes[newnode].title
+
   return (
     <Layout>
       <div>
-        <PostDate>{props.data.contentfulBlogPost.publishedDate}</PostDate>
+        <PostDate>
+          {props.data.contentfulBlogPost.publishedDate}
+          <br />
+          post {thisPostNode + 1} of {numberOfPosts}
+        </PostDate>
         <h3 className={blogPostStyles.title}>
           {titleCase(props.data.contentfulBlogPost.title)}
         </h3>
@@ -35,7 +77,13 @@ export default function BlogPost(props) {
           )}
         </p>
       </div>
-      {/* <PostNav></PostNav> */}
+
+      <PostNav
+        oldslug={oldslug}
+        newslug={newslug}
+        oldtitle={oldtitle}
+        newtitle={newtitle}
+      />
     </Layout>
   )
 }
--- src/templates/postnav.js
+++ src/templates/postnav.js
@@ -1,10 +1,27 @@
 import React from "react"
+import { Link } from "gatsby"
+import * as navStyles from "./navstyles.module.scss"
 
-export default function PostNav() {
+export default function PostNav(props) {
   return (
-    <div className="post-nav">
-      <div className="move newer">← newer</div>
-      <div className="move older">older →</div>
+    <div className={navStyles.postNav}>
+      <div className={navStyles.newer}>
+        <div>
+          <Link to={`/blog/${props.newslug}`}>
+            ← newer
+            <br />
+            {props.newtitle}
+          </Link>
+        </div>
+      </div>
+      <div className={navStyles.older}>
+        <div>
+          <Link to={`/blog/${props.oldslug}`}>
+            older →<br />
+            {props.oldtitle}
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
