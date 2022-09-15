86ea415edb5b1c6920ac7279c6e79a1d8d8f7734
Star-Squid <81830542+Star-Squid@users.noreply.github.com>
2022-09-15 10:14:39 +0100

    handle first and last element based on conditional in postnav

--- src/templates/postnav.js
+++ src/templates/postnav.js
@@ -3,34 +3,11 @@ import { Link } from "gatsby"
 import * as navStyles from "./navstyles.module.scss"
 
 export default function PostNav(props) {
-  if (props.last === true) {
-    return (
-      <div className={navStyles.postNav}>
-        <div className={navStyles.newer}>
-          <Link to={`/blog/${props.newslug}`}>
-            ← newer
-            <br />
-            {props.newtitle}
-          </Link>
-        </div>
-        <div className={navStyles.older}>&nbsp;</div>
-      </div>
-    )
-  } else if (props.first === true) {
-    return (
-      <div className={navStyles.postNav}>
+  return (
+    <div className={navStyles.postNav}>
+      {props.first === true ? (
         <div className={navStyles.newer}>&nbsp;</div>
-        <div className={navStyles.older}>
-          <Link to={`/blog/${props.oldslug}`}>
-            older →<br />
-            {props.oldtitle}
-          </Link>
-        </div>
-      </div>
-    )
-  } else {
-    return (
-      <div className={navStyles.postNav}>
+      ) : (
         <div className={navStyles.newer}>
           <Link to={`/blog/${props.newslug}`}>
             ← newer
@@ -38,13 +15,18 @@ export default function PostNav(props) {
             {props.newtitle}
           </Link>
         </div>
+      )}
+
+      {props.last === true ? (
+        <div className={navStyles.older}>&nbsp;</div>
+      ) : (
         <div className={navStyles.older}>
           <Link to={`/blog/${props.oldslug}`}>
             older →<br />
             {props.oldtitle}
           </Link>
         </div>
-      </div>
-    )
-  }
+      )}
+    </div>
+  )
 }
